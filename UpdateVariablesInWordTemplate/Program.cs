8bec4f484b92ca1864134d318ba867ede41f48e7
Allen Tuggle <allen@tuggleconsulting.net>
2023-01-14 12:03:44 -0500

    Updated file path code and added main try/catch

--- UpdateVariablesInWordTemplate/Program.cs
+++ UpdateVariablesInWordTemplate/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Net.NetworkInformation;
 using System.Text.RegularExpressions;
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
@@ -11,19 +12,47 @@ class Program
     static void Main(string[] args)
     {
         // Set the path to the Word document and the output.txt file
-        string filePath = @"C:\Users\allen\code\scratch\ReadingWritingWordDocument";
-        string templateName = @"Template1.docx";
-        //string outputPath = @"output.txt";
+        string templateFile = @"C:\Users\allen\code\scratch\ReadingWritingWordDocument\Template1.docx";
+        var saleItemFile = $@"C:\Users\allen\code\scratch\ReadingWritingWordDocument\output.txt";
 
-        var saleItem = new SaleItem() { Name = "John", Item = "Book", Price = "$15.00" };
-        SearchAndReplace(filePath, templateName, saleItem);
+        try
+        {
+            ReplaceVariablesInTemplate(templateFile, saleItemFile);
+        } 
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+    }
+
+    private static void ReplaceVariablesInTemplate(string templateFile, string saleItemFile)
+    {
+        var baseFolder = Path.GetDirectoryName(templateFile);
+
+        // Read the output.txt file line by line
+        using (var sr = new StreamReader(saleItemFile))
+        {
+            while (!sr.EndOfStream)
+            {
+                var line = sr.ReadLine();
+
+                // Split the line into key-value pairs
+                var keyValuePairs = line.Split('|');
+                var name = keyValuePairs[0];
+                var description = keyValuePairs[1];
+                var price = keyValuePairs[2];
+
+                var saleItem = new SaleItem() { Name = name, Item = description, Price = price };
+                SearchAndReplace(baseFolder, templateFile, saleItem);
+            }
+        }
     }
 
     // To search and replace content in a document part.
-    private static void SearchAndReplace(string filePath, string templateName, SaleItem saleItem)
+    private static void SearchAndReplace(string baseFolder, string templateFile, SaleItem saleItem)
     {
-        var templateFileName = $@"{filePath}{Path.DirectorySeparatorChar}{templateName}";
-        using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(templateFileName, true))
+        using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(templateFile, false))
         {
             string docText = null;
             using (StreamReader sr = new StreamReader(wordDoc.MainDocumentPart.GetStream()))
@@ -31,16 +60,16 @@ class Program
                 docText = sr.ReadToEnd();
             }
 
-            Regex regexText = new Regex("{name}");
-            docText = regexText.Replace(docText, saleItem.Name);
+            // Given this is a simple find/replace we do not need to load formal Open XML elements like Paragraphs, Runs, and Texts
+            docText = docText.Replace("{name}", saleItem.Name);
+            docText = docText.Replace("{description}", saleItem.Item);
+            docText = docText.Replace("{price}", saleItem.Price);
 
-            regexText = new Regex("{description}");
-            docText = regexText.Replace(docText, saleItem.Item);
+            // Save new document in an output folder with the name of the saleitem.name
+            var outputFolder = $@"{baseFolder}{Path.DirectorySeparatorChar}output";
+            EnsureFolderExists(outputFolder);
 
-            regexText = new Regex("{price}");
-            docText = regexText.Replace(docText, saleItem.Price);
-
-            var newFileName = $@"{filePath}{Path.DirectorySeparatorChar}{saleItem.Name}.docx";
+            var newFileName = $@"{outputFolder}{Path.DirectorySeparatorChar}{saleItem.Name}.docx";
             using (WordprocessingDocument newWordDoc = WordprocessingDocument.Create(newFileName, WordprocessingDocumentType.Document))
             {
                 // Add the main document part to the new document
@@ -51,7 +80,14 @@ class Program
                     sw.Write(docText);
                 }
             }
+        }
+    }
 
+    private static void EnsureFolderExists(string path)
+    {
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
         }
     }
 }
