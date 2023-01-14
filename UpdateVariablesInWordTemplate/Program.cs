ef5bb172d9df7fe944ff4159cd3e9d6c036daabd
Allen Tuggle <allen@tuggleconsulting.net>
2023-01-14 10:57:27 -0500

    Single line variables updating current doc working

--- UpdateVariablesInWordTemplate/Program.cs
+++ UpdateVariablesInWordTemplate/Program.cs
@@ -1,45 +1,63 @@
 ﻿using System;
 using System.IO;
-using Microsoft.Office.Interop.Word;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
 
 class Program
 {
     static void Main(string[] args)
     {
         // Set the path to the Word document and the output.txt file
-        string docPath = @"C:\example.docx";
-        string outputPath = @"C:\output.txt";
+        string docPath = @"C:\Users\allen\code\scratch\ReadingWritingWordDocument\Template1.docx";
+        string outputPath = @"C:\Users\allen\code\scratch\ReadingWritingWordDocument\output.txt";
 
-        // Create a new instance of the Word application
-        Application app = new Application();
+        ReadWriteWordDocument(docPath, outputPath);
+    }
 
-        // Open the Word document
-        Document doc = app.Documents.Open(docPath);
-
-        // Read in the output.txt file
-        string[] lines = File.ReadAllLines(outputPath);
-
-        // Iterate through each line in the output.txt file
-        foreach (string line in lines)
+    static void ReadWriteWordDocument(string wordDocument, string variableInputs)
+    {
+        using (WordprocessingDocument doc = WordprocessingDocument.Open(wordDocument, true))
         {
-            // Split the line into a key-value pair
-            string[] parts = line.Split('=');
-            string key = parts[0].Trim();
-            string value = parts[1].Trim();
+            var body = doc.MainDocumentPart.Document.Body;
+            var paras = body.Elements<Paragraph>();
 
-            // Replace all instances of the key surrounded by curly brackets in the document with the value
-            doc.Content.Find.ClearFormatting();
-            doc.Content.Find.Text = "{" + key + "}";
-            doc.Content.Find.Replacement.ClearFormatting();
-            doc.Content.Find.Replacement.Text = value;
-            doc.Content.Find.Execute(Replace: WdReplace.wdReplaceAll);
+            // Read the output.txt file line by line
+            using (var sr = new StreamReader(variableInputs))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+
+                    // Split the line into key-value pairs
+                    var keyValuePairs = line.Split('|');
+                    var name = keyValuePairs[0];
+                    var description = keyValuePairs[1];
+                    var price = keyValuePairs[2];
+
+                    // Replace all instances of the key surrounded by curly brackets in the Word document
+                    ReplaceText(paras, "{name}", name);
+                    ReplaceText(paras, "{description}", description);
+                    ReplaceText(paras, "{price}", price);
+                }
+            }
         }
+    }
 
-        // Save the modified document
-        doc.Save();
-
-        // Close the document and the Word application
-        doc.Close();
-        app.Quit();
+    private static void ReplaceText(IEnumerable<Paragraph> paras, string textToReplace, string replacementText)
+    {
+        foreach (var para in paras)
+        {
+            foreach (var run in para.Elements<Run>())
+            {
+                foreach (var text in run.Elements<Text>())
+                {
+                    if (text.Text.Contains(textToReplace))
+                    {
+                        text.Text = text.Text.Replace(textToReplace, replacementText);
+                    }
+                }
+            }
+        }
     }
 }
