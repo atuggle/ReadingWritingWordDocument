511b1366f517126498fa0c24ec3de0acf239dc2a
Allen Tuggle <allen@tuggleconsulting.net>
2023-01-14 11:53:10 -0500

    Replace and writing to new file working. Still need to read in variables

--- UpdateVariablesInWordTemplate/Program.cs
+++ UpdateVariablesInWordTemplate/Program.cs
@@ -1,63 +1,57 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
+using UpdateVariablesInWordTemplate;
 
 class Program
 {
     static void Main(string[] args)
     {
         // Set the path to the Word document and the output.txt file
-        string docPath = @"C:\Users\allen\code\scratch\ReadingWritingWordDocument\Template1.docx";
-        string outputPath = @"C:\Users\allen\code\scratch\ReadingWritingWordDocument\output.txt";
+        string filePath = @"C:\Users\allen\code\scratch\ReadingWritingWordDocument";
+        string templateName = @"Template1.docx";
+        //string outputPath = @"output.txt";
 
-        ReadWriteWordDocument(docPath, outputPath);
+        var saleItem = new SaleItem() { Name = "John", Item = "Book", Price = "$15.00" };
+        SearchAndReplace(filePath, templateName, saleItem);
     }
 
-    static void ReadWriteWordDocument(string wordDocument, string variableInputs)
+    // To search and replace content in a document part.
+    private static void SearchAndReplace(string filePath, string templateName, SaleItem saleItem)
     {
-        using (WordprocessingDocument doc = WordprocessingDocument.Open(wordDocument, true))
+        var templateFileName = $@"{filePath}{Path.DirectorySeparatorChar}{templateName}";
+        using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(templateFileName, true))
         {
-            var body = doc.MainDocumentPart.Document.Body;
-            var paras = body.Elements<Paragraph>();
-
-            // Read the output.txt file line by line
-            using (var sr = new StreamReader(variableInputs))
+            string docText = null;
+            using (StreamReader sr = new StreamReader(wordDoc.MainDocumentPart.GetStream()))
             {
-                while (!sr.EndOfStream)
+                docText = sr.ReadToEnd();
+            }
+
+            Regex regexText = new Regex("{name}");
+            docText = regexText.Replace(docText, saleItem.Name);
+
+            regexText = new Regex("{description}");
+            docText = regexText.Replace(docText, saleItem.Item);
+
+            regexText = new Regex("{price}");
+            docText = regexText.Replace(docText, saleItem.Price);
+
+            var newFileName = $@"{filePath}{Path.DirectorySeparatorChar}{saleItem.Name}.docx";
+            using (WordprocessingDocument newWordDoc = WordprocessingDocument.Create(newFileName, WordprocessingDocumentType.Document))
+            {
+                // Add the main document part to the new document
+                newWordDoc.AddMainDocumentPart();
+
+                using (StreamWriter sw = new StreamWriter(newWordDoc.MainDocumentPart.GetStream(FileMode.Create)))
                 {
-                    var line = sr.ReadLine();
-
-                    // Split the line into key-value pairs
-                    var keyValuePairs = line.Split('|');
-                    var name = keyValuePairs[0];
-                    var description = keyValuePairs[1];
-                    var price = keyValuePairs[2];
-
-                    // Replace all instances of the key surrounded by curly brackets in the Word document
-                    ReplaceText(paras, "{name}", name);
-                    ReplaceText(paras, "{description}", description);
-                    ReplaceText(paras, "{price}", price);
+                    sw.Write(docText);
                 }
             }
-        }
-    }
 
-    private static void ReplaceText(IEnumerable<Paragraph> paras, string textToReplace, string replacementText)
-    {
-        foreach (var para in paras)
-        {
-            foreach (var run in para.Elements<Run>())
-            {
-                foreach (var text in run.Elements<Text>())
-                {
-                    if (text.Text.Contains(textToReplace))
-                    {
-                        text.Text = text.Text.Replace(textToReplace, replacementText);
-                    }
-                }
-            }
         }
     }
 }
